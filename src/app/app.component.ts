0dee3601445bc7f3cb67283fd5236432ad2cd902
Simona Lorenc <lorenc.simona@gmail.com>
2024-08-26 11:26:22 +0200

    Add offer component and edit home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OfferComponent } from './offer/offer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, OfferComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
