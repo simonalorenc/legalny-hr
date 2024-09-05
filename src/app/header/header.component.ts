e99bbd389e5f30bba5e907611d866e9a9b1522ee
Simona Lorenc <lorenc.simona@gmail.com>
2024-09-05 14:21:10 +0200

    Add animations

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
@@ -7,17 +14,38 @@ import { Component } from '@angular/core';
   imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  animations: [
+    trigger('slideToggle', [
+      state('void', style({ height: '0', opacity: 0, overflow: 'hidden' })),
+      state('*', style({ height: '*', opacity: 1 })),
+      transition('void <=> *', [animate('300ms ease-in-out')]),
+    ]),
+    trigger('transparentBackground', [
+      state('true', style({ backgroundColor: 'transparent' })),
+      state('false', style({ backgroundColor: '#000000' })),
+      transition('* <=> *', animate(350)),
+    ]),
+  ],
 })
 export class HeaderComponent {
+  private TRANSPARENT_SCROLL_OFFSET: number = 40;
   isCollapsed: boolean = true;
+  isTransparent: boolean = true;
   toggleIcon: string = "header/menu.svg";
+
+  @HostListener('window: scroll', ['$event'])
+  private onScroll(event: Event): void {
+    if (!this.isCollapsed) return;
+    this.isTransparent = this.isTransparentScrollOffset();
+  }
 
   toggleCollapse() {
     this.isCollapsed = !this.isCollapsed;
-    if (this.isCollapsed) {
-      this.toggleIcon = "header/menu.svg";
-    } else if (!this.isCollapsed) {
-      this.toggleIcon = "header/close.svg";
-    }
+    this.toggleIcon = this.isCollapsed ? "header/menu.svg" : "header/close.svg";
+    this.isTransparent = this.isCollapsed && this.isTransparentScrollOffset();
+  }
+
+  private isTransparentScrollOffset(): boolean {
+    return window.scrollY < this.TRANSPARENT_SCROLL_OFFSET;
   }
 }
